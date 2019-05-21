3cedd3547bb6b117912cf2c053f5724b840b1f6f
Arthur Bureau <arthur.bureau@kedgebs.com>
2019-05-21 10:43:18 +0200

    authorization done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  #skip_before_action :authenticate_user!, only: :home
+  before_action :authenticate_user!
 end
--- app/controllers/monkeys_controller.rb
+++ app/controllers/monkeys_controller.rb
@@ -1,5 +1,5 @@
 class MonkeysController < ApplicationController
-  #skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @monkeys = Monkey.all
   end
