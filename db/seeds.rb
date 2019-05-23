86a000ea5ae23ca53b6e7d1c61eb4d1d6d684eff
Alexandrine Lelouch <lelouch.alexandrine@orange.fr>
2019-05-23 10:28:32 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Booking.destroy_all
+Monkey.destroy_all
 User.destroy_all
 
 
