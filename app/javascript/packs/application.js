a1bc1f33df1c51360acd7277becdbbb86f1437de
Arthur Bureau <arthur.bureau@kedgebs.com>
2019-05-23 18:36:43 +0200

    start sweet alert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,3 +5,18 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
+
+
+
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+initSweetalert('#alert-delete-monkey', {
+  title: "Are you sure?",
+  text: "This action cannot be reversed",
+  icon: "warning"
+}, (value) => {
+  if (value) {
+    const link = document.querySelector('#delete-link');
+    link.click();
+  }
+});
